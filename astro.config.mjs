1120f32425e09d087c6939dbcb67b0129801b9a6
Maximiliano Biondo <mbiondo@hcdn.gob.ar>
2024-04-29 21:40:41 -0300

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), sitemap(), robotsTxt()]
+});
