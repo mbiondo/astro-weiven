ea8e4d52f7262a4e35a902416dfb69cf6d516cef
Maximiliano Ivan Biondo <mbiondo@hcdn.gob.ar>
2024-04-30 15:05:35 -0300

    Update Astro config, package.json, and Tailwind config to add icon integration and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,16 @@ import tailwind from "@astrojs/tailwind"
 import sitemap from "@astrojs/sitemap"
 import robotsTxt from "astro-robots-txt"
 
+import icon from "astro-icon"
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), sitemap(), robotsTxt()],
+	integrations: [
+		tailwind(),
+		sitemap(),
+		robotsTxt(),
+		icon({
+			iconDir: "src/icons",
+		}),
+	],
 })
