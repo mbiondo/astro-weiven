f27f7b5b6abb7851bf770fdb7e1aa97a87252aec
Maximiliano Biondo <mbiondo@hcdn.gob.ar>
2024-04-30 00:50:24 -0300

    Update Astro config, package.json, and Tailwind config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
-import robotsTxt from "astro-robots-txt";
-
-import react from "@astrojs/react";
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import sitemap from "@astrojs/sitemap"
+import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), robotsTxt(), react()]
-});
+	integrations: [tailwind(), sitemap(), robotsTxt()],
+})
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -17,6 +17,5 @@ export default {
 		},
 		require("@tailwindcss/typography"),
 		require("@tailwindcss/aspect-ratio"),
-		require("@tailwindcss/line-clamp"),
 	],
 }
