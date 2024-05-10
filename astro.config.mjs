b9693e21efae6e0908027aa85e63989de3f3477f
Maximiliano Ivan Biondo <mbiondo@hcdn.gob.ar>
2024-05-10 19:10:00 -0300

    Update Astro config to add site URL and move icon directory

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,19 @@
-import { defineConfig } from "astro/config"
-import tailwind from "@astrojs/tailwind"
-import sitemap from "@astrojs/sitemap"
-import robotsTxt from "astro-robots-txt"
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import sitemap from '@astrojs/sitemap'
+import robotsTxt from 'astro-robots-txt'
 
-import icon from "astro-icon"
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://weiven.ar",
-	integrations: [
-		tailwind(),
-		sitemap(),
-		robotsTxt(),
-		icon({
-			iconDir: "src/icons",
-		}),
-	],
+  site: 'https://weiven.ar',
+  integrations: [
+    tailwind(),
+    sitemap(),
+    robotsTxt(),
+    icon({
+      iconDir: 'src/assets/icons',
+    }),
+  ],
 })
